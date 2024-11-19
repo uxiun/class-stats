51d8303092ce956a08e576bb3023839983cab6c1
uxiun <teppeiito0905@gmail.com>
2024-11-19 22:05:44 +0900

    w6 end, w7 start

--- w6six/for-exam.R
+++ w6six/for-exam.R
@@ -1,41 +1,43 @@
 
 
 # 非復元抽出
-	A <- rpois(100,50)
+	A <- rpois(100,50) # 平均50のポアソン分布、100個の無作為抽出標本
 	B <- rpois(100,52)
 
-	hist(A, breaks= 20, col= "#ff00ff40")
+	hist(A, breaks= 20, col= "#ff00ff40") # col表示histogram塗色
 	hist(B, breaks= 20, col= "#0000ff40", add= TRUE)
 	Diff <- abs(mean(A) - mean(B))
 	Data <- c(A,B)
 	data_r <- sample(Data, replace= FALSE) # 随机排序 replace=F: 非復元
-	
+
 	#新群
-	A_r <- data_r[1:length(A)] 
+	# A_r <- data_r[seq_along(length(A))]
+	A_r <- data_r[1:length(A)]
 	B_r <- data_r[(length(A)+1): length(data_r)]
-	
+
 	hist(A_r, breaks= 10, col= "#ff00ff40")
-	hist(B_r, breaks= 10, col= "#ff00ff40", add= TRUE)
-	
+	hist(B_r, breaks= 10, col= "#0000ff40", add= TRUE)
+
 	Diff_rs <- abs(mean(A_r) - mean(B_r))
-	
+
 	cat("Diff", Diff, "\n")
 	cat("Diff_rs", Diff_rs, "\n")
 
-# 随机化検定
-	n <- 4999
+# 随机化検定 Randomization
+	n <- 4999 # 迭代数
 	Diff_r <- numeric(n+1)
 	Data <- c(A,B)
-	
+
 	for (i in 1:n) {
 		data_r <- sample(Data, replace=F)
 		A_r <- data_r[1: length(A)]
 		B_r <- data_r[(length(A)+ 1): length(data_r)]
 		Diff_r[i] <- abs(mean(A_r) - mean(B_r))
 	}
-	
-	Diff_r[n+1] <- Diff 
-	largerCount <- sum(Diff_r >= Diff) # Diff_r.filter((r,i) => r >= Diff[i]).length に相当。i番目の要素がDiffのそれより大きいものを数える
+
+	Diff_r[n+1] <- Diff
+	largerCount <- sum(Diff_r >= Diff) # Diff_r.filter((r,i) => r >= Diff[i]).length に相当。i番目の要素がDiffのそれ以上のものを数える
+	# zip(Diff_r, Diff).filter((r, x) => r >= x).length
 	p <- largerCount/ (n+1)
 	cat("largerCount", largerCount, "\n")
 	cat("p", p, "\n")
--- w6six/report.R
+++ w6six/report.R
@@ -11,11 +11,11 @@
 # 	w <- example[,c]
 # 	m <- mean(w)
 # 	sd <- sd(w)
-	
+
 # 	cat(c, "の平均値=", m, "\n", sep="")
 # 	cat(c, "の標準偏差=", sd, "\n", sep="")
-	
-# 	ggplot(example[i], aes(x= w)) + 
+
+# 	ggplot(example[i], aes(x= w)) +
 # 		geom_histogram()+
 # 		labs(x= paste(c, "の体重", sep=""), y= "個体数") +
 # 				geom_segment(aes(x= m,    y= -0.3, xend= m, yend= 5), colour="red")+
@@ -38,20 +38,20 @@
 # 設問(2)
 	random_p <- function(n, example, showHist) {
 		Diff <- abs(mean(example[,1]) - mean(example[,2]))
-		
+
 		Diff_r <- numeric(n+1)
 		Data <- unlist(example, use.names=F)
-		
+
 		for (i in 1:n) {
 			data_r <- sample(Data, replace=F)
 			A_r <- data_r[1: length(example[,1])]
 			B_r <- data_r[(length(example[,2])+1): length(data_r)]
 			Diff_r[i] <- abs(mean(A_r) - mean(B_r))
 		}
-		
+
 		Diff_r[n+1] <- Diff
 		p <- sum(Diff_r >= Diff)/ (n+1)
-		
+
 		if (showHist) {
 			hist(Diff_r)
 			# arrows(p, -1, p, 0, code=3, col="red", lwd=2)
@@ -60,28 +60,28 @@
 		}
 		return(p)
 	}
-		
+
 	cat("p値", random_p(9999, example, T), "\n")
 
 # 設問(3)
-	
+
 	seq_step_resultlen <- function(start, end, resultlen) {
 		return(seq(start, end, (end - start)%/%(resultlen - 1)))
 	}
-	
+
 	pvalues <- sapply(1:10, function(i){
 		return(random_p(50, example, F))
 	} )
 	print("pvalues=")
 	print(pvalues)
-	
+
 	pmean <- mean(pvalues)
 	psd <- sd(pvalues)
 	cat("p値平均", pmean, "\n")
 	cat("p値標準偏差", psd, "\n")
-	
+
 	ptuples <- lapply( seq_step_resultlen(50, 50000, 11) , function(i) {
-		pvalues <- sapply(1:10, function(j) { 
+		pvalues <- sapply(1:10, function(j) {
 			return(random_p(i, example, FALSE))
 		})
 		pmean <- mean(pvalues)
@@ -89,12 +89,12 @@
 		return(c(i, pmean, psd))
 		# return(c(i, random_p(i, example, FALSE)) )
 	})
-	
+
 	print(ptuples)
-	
+
 	transposed <- transpose(ptuples)
 	print(transposed)
-	
+
 	values <- map(transposed,
 		function(x) { return(unlist(x, use.names= F)) }
 	)
@@ -103,11 +103,11 @@
 	)
 	print(flat)
 	print(values)
-	
+
 	# datas <- data.frame(values
 	# 	#, row.names= c("n", "p.mean", "p.sd")
 	# )
-	
+
 	# datas <- bind_rows(lapply(ptuples, as.data.frame.list))
 	# datas <- do.call(rbind.data.frame, flat)
 	datas <- dplyr::bind_rows(flat)
@@ -118,7 +118,7 @@
 	# 	# dat <- dplyr::bind_cols( c(data[aesxy[1]], data[aesxy[2]]) )
 
 	# 	print(data[[aesxy[1]]])
-		
+
 	# 	x <- data[[aesxy[1]]]
 	# 	y <- data[[aesxy[2]]]
 	# 	# dat <- data.frame(
@@ -138,25 +138,25 @@
 	# 	)
 	# 	return(gp)
 	# }
-	
+
 	# plotv(datas, c("n", "p_mean"), c(2, 3))+
 	# plotv(datas, c("n", "p_sd"), c(2, 3))
-	
+
 	# plot(x= datas$n, y= datas$p_mean, type="b")
 	# plot(x= datas$n, y= datas$p_sd, add= T, type="b" ,lty= 2)
-	
+
 	# datas %>%
 	# 	gather(ynames, cols, -n) %>%
 	# 	ggplot(aes(x= n, y= cols))+
 	# 	geom_line(aes(linetype= ynames))
-	
+
 	# print(gg)
-	
-	
+
+
 	ggplot(datas, aes(n, p_mean))+
 		geom_point()+
 		geom_line()
-	
+
 	ggplot(datas, aes(n, p_sd))+
 		geom_point()+
 		geom_line(linetype= "dashed")
@@ -164,13 +164,13 @@
 
 # 設問(4)
 	res.perm <- permTS(
-		x= example$A, 
-		y= example$B, 
+		x= example$A,
+		y= example$B,
 		alternative= "greater",
-	
+
 	)
 	print(res.perm)
-	
+
 	res.t <- t.test(
 		x= example$A,
 		y= example$B,
@@ -180,4 +180,4 @@
 		conf.level= 0.95,
 	)
 	print(res.t)
-	
+
